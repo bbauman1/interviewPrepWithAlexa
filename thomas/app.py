28e5e8720e9e4cd04160bc16c6f7ee6f393fa144
colmbus72 <cjdudley@asu.edu>
2017-01-14 10:47:33 -0700

    Prolly gonna be an error

--- thomas/app.py
+++ thomas/app.py
@@ -22,6 +22,46 @@ def get_difficulty(diff):
 	real_q = questions[randint(0, len(questions) - 1)]
 	return real_q
 
+def start_time(minutes, questionTitle):
+	seconds = minutes*60
+	CHUNK = 1024
+	FORMAT = pyaudio.paInt16
+	CHANNELS = 2
+	RATE = 44100
+	RECORD_SECONDS = seconds
+	WAVE_OUTPUT_FILENAME = questionTitle+".wav"
+	
+	p = pyaudio.PyAudio()
+	
+	stream = p.open(format=FORMAT,
+	                channels=CHANNELS,
+	                rate=RATE,
+	                input=True,
+	                frames_per_buffer=CHUNK)
+	
+	print("* recording")
+	
+	frames = []
+	
+	for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+	    data = stream.read(CHUNK)
+	    frames.append(data)
+	
+	print("* done recording")
+	
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+	
+	wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+	wf.setnchannels(CHANNELS)
+	wf.setsampwidth(p.get_sample_size(FORMAT))
+	wf.setframerate(RATE)
+	wf.writeframes(b''.join(frames))
+	wf.close()
+
+
+
 @ask.launch
 def welcome_intern():
     return question(render_template('welcome'))
@@ -39,6 +79,7 @@ def question_type_difficulty(Diff):
 	q = get_difficulty(norm_difficulty)
 
 	session.attributes['company'] = q 
+	start_timer(20)
 
 	return question(q['description'] + 'Would you like me to repeat the question?')
 
