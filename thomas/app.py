118f400d36edc45e6bcc023c34a1972e00237f22
colmbus72 <cjdudley@asu.edu>
2017-01-14 06:26:24 -0700

    Added voice recording

--- thomas/app.py
+++ thomas/app.py
@@ -3,6 +3,8 @@ from random import randint
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
 
+import pyaudio
+import wave
 
 app = Flask(__name__)
 
@@ -10,6 +12,42 @@ ask = Ask(app, "/")
 
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
+def startTimer(time):	
+	CHUNK = 1024
+	FORMAT = pyaudio.paInt16
+	CHANNELS = 2
+	RATE = 44100
+	RECORD_SECONDS = time
+	WAVE_OUTPUT_FILENAME = "interViewRecording.wav"
+
+	p = pyaudio.PyAudio()
+
+	stream = p.open(format=FORMAT,
+	                channels=CHANNELS,
+	                rate=RATE,
+	                input=True,
+	                frames_per_buffer=CHUNK)
+	
+	print("* recording")
+	
+	frames = []
+	
+	for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+	    data = stream.read(CHUNK)
+	    frames.append(data)
+	
+	print("* done recording")
+	
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+	
+	wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+	wf.setnchannels(CHANNELS)
+	wf.setsampwidth(p.get_sample_size(FORMAT))
+	wf.setframerate(RATE)
+	wf.writeframes(b''.join(frames))
+	wf.close()
 
 @ask.launch
 def welcome_intern():
@@ -39,6 +77,7 @@ def question_type_difficulty(Diff):
 	norm_difficulty = Diff.lower().strip()
 	print norm_difficulty
 	if norm_difficulty == 'easy':
+		startTimer(20);
 		return statement('Easy question')
 	elif norm_difficulty == 'medium':
 		return statement('Medium question')
@@ -52,3 +91,5 @@ def help_intent():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
